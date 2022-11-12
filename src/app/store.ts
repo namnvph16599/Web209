a424433c65384d7e9ee5b62a5dab50b6d9190efa
Ngô Vưn Nam <anh02284@gmail.com>
2022-11-12 23:14:25 +0700

    setting json server auth

---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counter from './slices/counter'
+import AuthSlices from './slices/auth'
+export const store = configureStore({
+    reducer: {
+        counterSlice: counter,
+        auth: AuthSlices
+    },
+})
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>;
---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+const PORT = 'http://localhost:3000/';
+export const PORT_USER = `${PORT}/users`;
